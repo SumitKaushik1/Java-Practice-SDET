d8ebefa74fc243945a7b7306e82f75002429380e
sumit kaushik <110432346+SumitKaushik1@users.noreply.github.com>
2023-09-15 16:23:24 +0530

    Delete src directory

--- src/DayOfWeek.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
-
-public class DayOfWeek {
-    public static void main(String[] args) {
-        System.out.println( LocalDate.of(2016,1,23).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH).toUpperCase());
-    }
-}
