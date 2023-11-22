101056b73e043d1f8f27622454fe4f7206a2fab6
sumit <pritam.2581996@gmail.com>
2023-11-22 14:06:53 +0530

    new

---
+++ src/ExceptionWithBitwiseOrSignInCatchBlock.java
@@ -0,0 +1,17 @@
+public class ExceptionWithBitwiseOrSignInCatchBlock {
+    public static void main(String[] args) {
+
+    try{
+
+
+    }catch(NullPointerException|ArithmeticException e){
+
+    }
+
+
+
+}
+
+
+
+}
---
+++ src/NextPrimeNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NextPrimeNumber {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num=scan.nextInt();
+
+        char [] c=String.valueOf(num).toCharArray();
+
+        for(int i=0;i<c.length;i++){
+            int digit=Integer.valueOf(c[i]);
+            int i=digit;
+            while(true){
+
+                //logic prime number if first prime number come break
+
+                i++;
+            }
+
+            //print that here 
+
+
+        }
+
+
+        // digits take out
+        //store in the array
+
+
+        //prime number logic
+    }
+}
